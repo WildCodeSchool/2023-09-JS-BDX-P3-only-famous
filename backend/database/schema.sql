750c8b3d172988f3f2495bef39961b13de6ca140
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2023-12-05 20:41:22 +0100

    adding env

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,6 @@
-create table item (
+create table user (
   id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+  username varchar(255) not null,
+  firstname varchar(255) not null,
+  lastname varchar(255) not null
 );
