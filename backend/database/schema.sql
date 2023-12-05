36f66bd0ade3bc249e27fbc63dd837f494613360
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2023-12-05 22:52:22 +0100

    adding bootstrap

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -2,5 +2,7 @@ create table user (
   id int unsigned primary key auto_increment not null,
   username varchar(255) not null,
   firstname varchar(255) not null,
-  lastname varchar(255) not null
+  lastname varchar(255) not null,
+  birthday DATE NOT NULL,
+  isAdming TINYINT(1) DEFAULT 0
 );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import App from "./App";
 
