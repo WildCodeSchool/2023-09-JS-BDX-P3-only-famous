4e59b992a138b5a11fff6a52bcb526ccddec52e6
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2023-12-08 23:24:37 +0100

    adding navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,89 +1,10 @@
 import "./App.css";
-import {
-  MDBAccordion,
-  MDBAccordionItem,
-  MDBBtn,
-  MDBCarousel,
-  MDBCarouselItem,
-  MDBCarouselCaption,
-} from "mdb-react-ui-kit";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <MDBBtn>Button</MDBBtn>
-      <MDBAccordion initialActive={1}>
-        <MDBAccordionItem collapseId={1} headerTitle="Accordion Item #1">
-          <strong>This is the first item's accordion body.</strong> It is shown
-          by default, until the collapse plugin adds the appropriate classes
-          that we use to style each element. These classes control the overall
-          appearance, as well as the showing and hiding via CSS transitions. You
-          can modify any of this with custom CSS or overriding our default
-          variables. It's also worth noting that just about any HTML can go
-          within the <code>.accordion-body</code>, though the transition does
-          limit overflow.
-        </MDBAccordionItem>
-        <MDBAccordionItem collapseId={2} headerTitle="Accordion Item #2">
-          <strong>This is the second item's accordion body.</strong> It is
-          hidden by default, until the collapse plugin adds the appropriate
-          classes that we use to style each element. These classes control the
-          overall appearance, as well as the showing and hiding via CSS
-          transitions. You can modify any of this with custom CSS or overriding
-          our default variables. It's also worth noting that just about any HTML
-          can go within the <code>.accordion-body</code>, though the transition
-          does limit overflow.
-        </MDBAccordionItem>
-        <MDBAccordionItem collapseId={3} headerTitle="Accordion Item #3">
-          <strong>This is the third item's accordion body.</strong> It is hidden
-          by default, until the collapse plugin adds the appropriate classes
-          that we use to style each element. These classes control the overall
-          appearance, as well as the showing and hiding via CSS transitions. You
-          can modify any of this with custom CSS or overriding our default
-          variables. It's also worth noting that just about any HTML can go
-          within the <code>.accordion-body</code>, though the transition does
-          limit overflow.
-        </MDBAccordionItem>
-      </MDBAccordion>
-
-      <MDBCarousel showIndicators showControls fade>
-        <MDBCarouselItem itemId={1}>
-          <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg"
-            className="d-block w-100"
-            alt="..."
-          />
-          <MDBCarouselCaption>
-            <h5>First slide label</h5>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </MDBCarouselCaption>
-        </MDBCarouselItem>
-
-        <MDBCarouselItem itemId={2}>
-          <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(22).jpg"
-            className="d-block w-100"
-            alt="..."
-          />
-          <MDBCarouselCaption>
-            <h5>Second slide label</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </MDBCarouselCaption>
-        </MDBCarouselItem>
-
-        <MDBCarouselItem itemId={3}>
-          <img
-            src="https://mdbootstrap.com/img/Photos/Slides/img%20(23).jpg"
-            className="d-block w-100"
-            alt="..."
-          />
-          <MDBCarouselCaption>
-            <h5>Third slide label</h5>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </MDBCarouselCaption>
-        </MDBCarouselItem>
-      </MDBCarousel>
+      <Navbar />
     </div>
   );
 }
