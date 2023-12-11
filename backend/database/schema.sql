db6e7b5523870208fec7dd6e6426718f416541b7
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2023-12-11 11:06:51 +0100

    database

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,8 +1,5 @@
-create table (
+create table
+    user(
         id int unsigned primary key auto_increment not null,
-        username varchar(255) not null,
-        firstname varchar(255) not null,
-        lastname varchar(255) not null,
-        birthday DATE NOT NULL,
-        isAdming TINYINT(1) DEFAULT 0
+        title varchar(255) not null,
     );
