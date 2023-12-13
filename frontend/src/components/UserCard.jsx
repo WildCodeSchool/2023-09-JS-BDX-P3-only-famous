7f4e2a47a82ec1ce5f73e1a6d8347dee9edfc3b4
Mathieu Chauveau <chauveau.math@gmail.com>
2023-12-13 16:31:55 +0100

    debut page admin + data users + userCard

---
+++ frontend/src/components/UserCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UserCard() {
+  return (
+    <div>
+      <h1>Name</h1>
+      <h2>LastName</h2>
+    </div>
+  );
+}
+
+export default UserCard;
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -2,14 +2,39 @@ import React from "react";
 
 function Admin() {
   return (
-    <>
-      <div>
-        <h1>Hello</h1>
+    <div className="admin_container">
+      <div className="filtre_recherche">
+        <ul>
+          <li>
+            <button type="button">Utilisateurs</button>
+          </li>
+          <li>
+            <button type="button">Videos</button>
+          </li>
+          <li>
+            <input type="search" />
+          </li>
+        </ul>
       </div>
-      <div>
-        <h2>Yo</h2>
+      <div className="users_container">
+        <div className="users_infos">
+          <ul>
+            <li>Id</li>
+            <li>Nom</li>
+            <li>Prénom</li>
+            <li>Date de naissance</li>
+            <li>Mail</li>
+            <li>Date d'inscription</li>
+            <li>Admin</li>
+          </ul>
+        </div>
+        {/*  <div className="users_data">
+          {usersData.map((item) => (
+            <UserCard firstname={item.firstname} lastname={item.lastname} />
+          ))}
+        </div> */}
       </div>
-    </>
+    </div>
   );
 }
 
