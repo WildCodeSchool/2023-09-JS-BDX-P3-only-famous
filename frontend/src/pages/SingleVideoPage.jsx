cdddc45a1dabc072e8bbddb53e803895c739a486
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2023-12-20 10:38:22 +0100

    deleting alerts

--- frontend/src/pages/SingleVideoPage.jsx
+++ frontend/src/pages/SingleVideoPage.jsx
@@ -1,14 +1,12 @@
 import Carroussel from "../components/Carroussel";
 import OneVideo from "./OneVideo";
 import videoItems from "../components/Data";
-import Separator from "../components/Separator";
 
 export default function SingleVideoPage() {
   return (
     <div className="singleVideoPage">
       <OneVideo />
       <Carroussel videoItems={videoItems} titre="Favories" />
-      <Separator />
     </div>
   );
 }
