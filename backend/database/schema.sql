618713923d95e6a6c3ef19700027b766ce988386
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2023-12-20 09:59:31 +0100

    fixing back-end

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -3,11 +3,33 @@ create table
         id int unsigned primary key auto_increment not null,
         firstname varchar(255) not null,
         lastname VARCHAR(255) not null,
-        email varchar(255) not null,
+        email varchar(255) not null UNIQUE,
         birthday VARCHAR(255) not null,
         password VARCHAR(255) not null,
         isAdmin BOOLEAN DEFAULT false
     );
-    insert INTO user (firstname, lastname, email, password, birthday, isAdmin)
-        VALUES ("Mahdi","Mcheik", "mahdi.mcheik@hotmail.fr", "1234","21-04-1986", true), 
-        ("Ali","Mcheik", "mahdi.mcheik@hotmail.fr", "1234","21-04-1986", false);
+
+insert INTO
+    user (
+        firstname,
+        lastname,
+        email,
+        password,
+        birthday,
+        isAdmin
+    )
+VALUES (
+        "Mahdi",
+        "Mcheik",
+        "mahdi.mcheik@hotmail.fr",
+        "1234",
+        "21-04-1986",
+        true
+    ), (
+        "Ali",
+        "Mcheik",
+        "mahdi.mcheik@hotmail.com",
+        "1234",
+        "21-04-1986",
+        false
+    );
