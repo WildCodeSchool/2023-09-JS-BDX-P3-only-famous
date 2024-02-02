8d65e4c55b83f7853bfee19ac813f6ebab360503
mahdi mcheik <mahdi.mcheik@hotmail.fr>
2024-02-02 15:20:46 +0100

    update description userupdae description of user

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -19,6 +19,7 @@ CREATE TABLE if not exists `user` (
   `isActive` tinyint(1) DEFAULT '0',
   `imgUrl` varchar(255) DEFAULT NULL,
   `activationCode` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT null,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
