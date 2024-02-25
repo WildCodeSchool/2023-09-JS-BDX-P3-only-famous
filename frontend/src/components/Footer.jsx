956755eed976400053f63238ba06c5c7e4168241
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2024-02-25 22:02:34 +0100

    fixing  image profil, footer main text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="footer-footer">
-      <div className="text-center">© 2023 World Discovery</div>
+      <div className="text-center">© 2024 la Banque de Tutos</div>
     </div>
   );
 }
