a32a5763ad6ff829d88104e0182c250558d9ccd2
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2024-02-25 23:08:38 +0100

    last try

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -137,10 +137,7 @@ export default function UserContextProvider({ children }) {
     try {
       const { message, insertId } = await axios.post(
         `${import.meta.env.VITE_BACKEND_URL}/api/users`,
-        {
-          ...newUser,
-          imgUrl: `${import.meta.env.VITE_BACKEND_URL}/uploads/charlize.jpg`,
-        }
+        newUser
       );
       if (+insertId === 0) {
         setMessageUser(message);
